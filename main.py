7685bb1516682de9c6dff54e4e7c4e773f0186dd
SelloutXD <86364206+SelloutXD@users.noreply.github.com>
2022-04-27 19:55:04 +0300

    restored extensions

---
+++ main.py
@@ -0,0 +1,60 @@
+import os
+import argparse
+from urllib.parse import urlparse
+
+from dotenv import load_dotenv
+import requests
+
+
+def shorten_link(parameters, url):
+    body = {
+        "long_url": url,
+    }
+    response = requests.post(
+        "https://api-ssl.bitly.com/v4/bitlinks",
+        headers=parameters,
+        json=body
+    )
+    response.raise_for_status()
+    return response.json()['link']
+
+
+def count_clicks(parameters, url):
+    response = requests.get(
+        f"https://api-ssl.bitly.com/v4/bitlinks/{url}/clicks/summary",
+        headers=parameters
+    )
+    response.raise_for_status()
+    return response.json()['total_clicks']
+
+
+def is_bitlink(url, parameters):
+    response = requests.get(
+        f"https://api-ssl.bitly.com/v4/bitlinks/{url}/clicks/summary",
+        headers=parameters
+    )
+    return response.ok
+
+
+if __name__ == "__main__":
+    load_dotenv()
+    parser = argparse.ArgumentParser(description='Shortens links')
+    parser.add_argument('--url', help='Введите ссылку')
+    args = parser.parse_args()
+    url = args.url
+    token = os.environ["SECRET_BITLY_TOKEN"]
+    parameters = {
+        "Authorization": f"Bearer {token}",
+    }
+    parsed_url = urlparse(url)
+    url_without_scheme = f"{parsed_url.netloc}{parsed_url.path}"
+    if is_bitlink(url_without_scheme, parameters):
+        try:
+            print("amount of clicks:", count_clicks(parameters, url_without_scheme))
+        except requests.exceptions.HTTPError:
+            print("click counting error")
+    else:
+        try:
+            print("your shortend link:", shorten_link(parameters, url))
+        except requests.exceptions.HTTPError:
+            print("link shortening error")
