b8937237c4ec9c0a97f7caf4a827ef7811fec9d2
SelloutXD <86364206+SelloutXD@users.noreply.github.com>
2022-05-16 18:47:24 +0300

    changed parameters to headers

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from dotenv import load_dotenv
 import requests
 
 
-def shorten_link(parameters, url):
+def shorten_link(headers, url):
     body = {
         "long_url": url,
     }
@@ -19,19 +19,19 @@ def shorten_link(parameters, url):
     return response.json()['link']
 
 
-def count_clicks(parameters, url):
+def count_clicks(headers, url):
     response = requests.get(
         f"https://api-ssl.bitly.com/v4/bitlinks/{url}/clicks/summary",
-        headers=parameters
+        headers=headers
     )
     response.raise_for_status()
     return response.json()['total_clicks']
 
 
-def is_bitlink(url, parameters):
+def is_bitlink(url, headers):
     response = requests.get(
         f"https://api-ssl.bitly.com/v4/bitlinks/{url}/clicks/summary",
-        headers=parameters
+        headers=headers
     )
     return response.ok
 
@@ -43,18 +43,18 @@ if __name__ == "__main__":
     args = parser.parse_args()
     url = args.url
     token = os.environ["SECRET_BITLY_TOKEN"]
-    parameters = {
+    headers = {
         "Authorization": f"Bearer {token}",
     }
     parsed_url = urlparse(url)
     url_without_scheme = f"{parsed_url.netloc}{parsed_url.path}"
-    if is_bitlink(url_without_scheme, parameters):
+    if is_bitlink(url_without_scheme, headers):
         try:
-            print("amount of clicks:", count_clicks(parameters, url_without_scheme))
+            print("amount of clicks:", count_clicks(headers, url_without_scheme))
         except requests.exceptions.HTTPError:
             print("click counting error")
     else:
         try:
-            print("your shortend link:", shorten_link(parameters, url))
+            print("your shortend link:", shorten_link(headers, url))
         except requests.exceptions.HTTPError:
             print("link shortening error")
